60dc126799f27fdf19eb815faf00994f1d4f2739
Flaim92 <115178675+Flaim92@users.noreply.github.com>
2022-10-07 18:45:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+link = 'https://vk.com/music/album/-2000272205_16272205_75c9544641e855d152'
+login = ''
+password = ''
+login2 = ''
+password2 = ''
+login3 = ''
+password3 = ''
+login4 = ''
+password4 = ''
+
+
